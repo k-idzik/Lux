30bf9c0f88ccd44eaad845901c24243d5726430b
jayshuart <joelshuart@gmail.com>
2018-04-08 22:06:25 -0400

    turnable searchlight (just hold Space and run into it)
    uses physics

---
+++ Assets/Scripts/SearchLight.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SearchLight : MonoBehaviour {
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = this.gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Turn();
+	}
+
+    //Turn the spotlight
+    public void Turn()
+    {
+        //check if player is hitting interact button
+        if (Input.GetKey(KeyCode.Space))
+        {
+            //freeze pos and xz rot
+            rb.constraints = RigidbodyConstraints.FreezePosition | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationX;
+        }
+        else
+        {
+            //freeze spotlight
+            rb.constraints = RigidbodyConstraints.FreezeAll;
+        }
+    }
+}
