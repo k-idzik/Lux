148fc0a2022e045545c21b7af303cc233264eb60
Charles Williams (RIT Student) <crw4147@ad.rit.edu>
2018-04-22 18:18:45 -0400

    Cleaned up enemy code

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,6 +5,8 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+    // enemy variables
+
     // determines how the patrol behaves when it reaches its final node
     // LOOP: will restart at the beginning of its route, creating a loop
     // REVERSE: will visit all nodes in reverse order, good for lines
@@ -12,35 +14,33 @@ public class Enemy : MonoBehaviour
     public PatrolType patrolBehavior;
 
     // cooldown between when enemy searches for player again
-    [SerializeField]
-    private float searchCooldown;
+    public float searchCooldown;
 
-    // enemy variables
-    private Animator enemyAnim;
-    [SerializeField]
+    // angle of the vision cone, will extend this angle in both directions
+    public float visionAngle;
+
+    // maximum range of vision cone
+    public float visionConeRange;
+   
+    // general enemy variables
     private GameObject target;
-    [SerializeField]
-    private float visionAngle;
     private NavMeshAgent agent;
     private DetectionSphere detectionSphere;
-    [SerializeField]
-    private List<GameObject> patrolRoute;
-    [SerializeField]
-    private GameObject alertPoint;
+
+    // route enemy will patrol on
+    public List<GameObject> patrolRoute;
+
+    // variables for tracking enemy state
+    private enum State { PATROL, PURSUE};
+    private State currentState;
     private List<bool> visitedWaypoints;
     private GameObject currentWaypoint;
-    private bool reverse;
+    private bool reverse; // for detecting if we're doing the route in reverse, not the patrol state reverse
     private bool patrol;
     private bool pursuit;
-    private bool alert;
     private bool scanning = false;
-    private bool alertScan = false;
     private bool finished = false;
     private bool running = false;
-    private Vector3 lastKnownLoc = Vector3.zero;
-
-    public AudioClip enemyMove1;
-    public AudioClip enemyMove2;
 
     public bool Pursuit
     {
@@ -52,18 +52,15 @@ public class Enemy : MonoBehaviour
 
     // Use this for initialization
     void Start()
-    {
+    { 
+        // setup
         agent = GetComponent<NavMeshAgent>();
         detectionSphere = GetComponentInChildren<DetectionSphere>();
-        Debug.Log(patrolRoute.Count);
-
+        target = GameObject.FindGameObjectWithTag("Player");
         visitedWaypoints = new List<bool>();
-
         currentWaypoint = patrolRoute[0];
-
         reverse = false;
-
-        patrol = true;
+        currentState = State.PATROL;
 
         // assign a default value of false for each waypoint in the patrol route
         for (int i = 0; i < patrolRoute.Count; i++)
@@ -73,29 +70,18 @@ public class Enemy : MonoBehaviour
     }
 
     /// <summary>
-    /// responsible for seeking the enemy's target.
-    /// </summary>
-    public Vector3 Seek()
-    {
-        Vector3 offset = target.transform.position - this.transform.position;
-        Vector3 unitOffset = offset.normalized;
-        return unitOffset;
-    }
-
-    /// <summary>
     /// responsible for detecting the player based on
     /// the cone of vision and checking 
     /// </summary>
     private bool VisionCone()
     {
-        target = GameObject.FindGameObjectWithTag("Player");
-
         Vector3 direction = target.transform.position - this.transform.position;
 
         float angle = Vector3.Angle(direction, transform.forward);
 
-        Debug.DrawLine(transform.position, transform.position + Quaternion.AngleAxis(visionAngle, transform.up) * transform.forward * 20);
-        Debug.DrawLine(transform.position, transform.position + Quaternion.AngleAxis(-visionAngle, transform.up) * transform.forward * 20);
+        Debug.DrawLine(transform.position, transform.position + Quaternion.AngleAxis(visionAngle, transform.up) * transform.forward * visionConeRange);
+        Debug.DrawLine(transform.position, transform.position + Quaternion.AngleAxis(-visionAngle, transform.up) * transform.forward * visionConeRange);
+        Debug.DrawLine(transform.position, transform.position + transform.forward * visionConeRange);
 
         if (angle > visionAngle)
         {
@@ -105,7 +91,7 @@ public class Enemy : MonoBehaviour
         {
             RaycastHit hit;
            
-            Physics.Raycast(transform.position, direction, out hit);
+            Physics.Raycast(transform.position, direction, out hit, visionConeRange);
 
             if (hit.transform.gameObject == target)
             {
@@ -161,6 +147,12 @@ public class Enemy : MonoBehaviour
                 visitedWaypoints[i] = false;
             }
         }
+
+        // player detection
+        if (VisionCone() || detectionSphere.PlayerDetected)
+        {
+            currentState = State.PURSUE;
+        }
     }
 
     /// <summary>
@@ -209,21 +201,10 @@ public class Enemy : MonoBehaviour
                 }
             }
         }
-
-        if (coll.transform.tag == "AlertPoint" && alert == true)
-        {
-            alertScan = true;
-            Destroy(coll.gameObject);
-        }
     }
 
     public void OnTriggerStay(Collider coll)
     {
-        if (coll.transform.tag == "AlertPoint" && alert == true)
-        {
-            alertScan = true;
-            Destroy(coll.gameObject);
-        }
         if (coll.transform.tag == "Waypoint" && coll.gameObject == currentWaypoint && reverse == false)
         {
             for (int i = 0; i < patrolRoute.Count; i++)
@@ -277,8 +258,6 @@ public class Enemy : MonoBehaviour
     {
         if (finished == false)
         {
-           
-            Debug.Log("WORK");
             Debug.DrawLine(transform.position,target.transform.position,Color.red);
             agent.destination = GameObject.FindGameObjectWithTag("Player").transform.position;
             if (running == false)
@@ -290,98 +269,30 @@ public class Enemy : MonoBehaviour
         if (finished == true)
         {
             finished = false;
-            pursuit = false;
-            patrol = true;
+            currentState = State.PATROL;
         }
     }
 
     internal IEnumerator Chase(float duration)
     {
         running = true;
-
         yield return new WaitForSeconds(duration);
         finished = true;
         running = false;
     }
 
-    /// <summary>
-    /// if player is spotted on patrol enemies will go to last
-    /// known location and look for player, if they detect them,
-    /// then pursuit will begin
-    /// </summary>
-    /// <param name="location"></param>
-    //private void Alert(Vector3 location)
-    //{
-    //    agent.destination = location;
-
-    //    if (alertScan)
-    //    {
-    //        agent.Stop();
-            
-    //        Coroutine sa =StartCoroutine(Scan(3));
-
-    //        if (scanning == true)
-    //        {
-    //            if (VisionCone() || detectionSphere.PlayerDetected)
-    //            {
-    //                StopCoroutine(sa);
-    //                alert = false;
-    //                pursuit = true;
-    //                scanning = false;
-    //                alertScan = false;
-    //            }
-    //        }
-    //    }
-    //}
-
-    //internal IEnumerator Scan(float duration)
-    //{
-    //    scanning = true;
-    //    visionAngle = 60;
-
-    //    yield return new WaitForSeconds(duration);
-
-    //    visionAngle = 20;
-    //    scanning = false;
-    //    alertScan = false;
-    //    alert = false;
-    //    patrol = true;
-    //}
-
- 
-
     // Update is called once per frame
     void Update()
     {
-        //enemyAnim.SetBool("PursuitAnim", pursuit);
-
-        if (patrol)
+        switch (currentState)
         {
-            Patrol();
-       
-            if (VisionCone() || detectionSphere.PlayerDetected)
-            {
-                //SoundManager.instance.RandomizeEnemySfx(enemyMove1);
-                //alert = true;
-                pursuit = true;
-                patrol = false;
-                //lastKnownLoc = GameObject.FindGameObjectWithTag("Player").transform.position;
+            case State.PATROL:
+                Patrol();
+                break;
 
-                //GameObject.Instantiate(alertPoint, lastKnownLoc, Quaternion.identity);
-            }
+            case State.PURSUE:
+                Pursue();
+                break;
         }
-
-        if (pursuit)
-        {
-          
-            Pursue();
-            //Debug.Log(pursuit);
-        }
-
-        //if (alert)
-        //{
-        //    Alert(lastKnownLoc);
-        //}
-
     }
 }
