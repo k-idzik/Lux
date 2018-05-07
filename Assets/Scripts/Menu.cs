4ff1cce226a3fb660efeac6378554ddbed05ef8a
Kevin Idzik <kdi1996@live.com>
2018-05-06 21:53:29 -0400

    Updated credits

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 
 public class Menu : MonoBehaviour {
     private bool showingCredits;
-    [SerializeField] private Text creditsTxt;
+    [SerializeField] private TextMesh creditsTxt;
 
 	// Use this for initialization
 	void Start () {
@@ -34,15 +34,26 @@ public class Menu : MonoBehaviour {
     {
         //update credits bool
         showingCredits = !showingCredits;
+        Debug.Log(showingCredits);
 
         //update text
         if (showingCredits)
         {
-            creditsTxt.text = "Joel, Charles, Josh, John, and Kevin";
+            //creditsTxt.text = "Joel, Charles, Josh, John, and Kevin";
+            //creditsTxt.enabled = true;
+            creditsTxt.text = "Kevin Idzik\nJosh Malmquist\nJohn Palermo\nJoel Shuart\nCharles Williams";
+            creditsTxt.fontSize = 85;
+            creditsTxt.lineSpacing = .75f;
+            creditsTxt.transform.position = new Vector3(creditsTxt.transform.position.x, 45, creditsTxt.transform.position.z);
         }
         else
         {
-            creditsTxt.text = "Credits";
+            //creditsTxt.text = "Credits";
+            //creditsTxt.enabled = false;
+            creditsTxt.text = "LUX";
+            creditsTxt.fontSize = 500;
+            creditsTxt.lineSpacing = 1;
+            creditsTxt.transform.position = new Vector3(creditsTxt.transform.position.x, -1, creditsTxt.transform.position.z);
         }
     }
 
